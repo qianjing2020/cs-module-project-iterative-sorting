f0e8143e19dadce79dbef7e280982684037658d1
Jing Qian <qianjing2020@users.noreply.github.com>
2020-07-20 22:39:51 -0400

    initial commit

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,22 +6,43 @@ def selection_sort(arr):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
-        # Your code here
 
-
-        # TO-DO: swap
-        # Your code here
+        min_idx = arr.index(min(arr[i:]))
+        # swap
+        a = arr[min_idx]
+        arr[min_idx] = arr[smallest_index]
+        arr[smallest_index] = a
 
     return arr
 
+import random   
+array = list(range(0, 9))
+random.shuffle(array)
+print(array)
+print(selection_sort(array))
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
-
+    # make a series of swaps between adjacent elements, gradually moving larger elements towards the end of the array (or bubbling larger elements up)
+    # loop through n-1 elements
+    arr_index = list(range(0, len(arr)))
+    end_sort_index = arr_index[-1::-1] 
+    for end_sort in end_sort_index:
+        for i in range(0, end_sort):
+                    
+            # if current index value is bigger, bubble toward right     
+            if arr[i] > arr[i+1]:
+                # swap
+                a = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = a
 
     return arr
 
+
+random.shuffle(array)
+print(array)
+print(bubble_sort(array))
 '''
 STRETCH: implement the Counting Sort function below
 
@@ -41,6 +62,33 @@ What is the time and space complexity of the counting sort algorithm?
 '''
 def counting_sort(arr, maximum=None):
     # Your code here
+    if maximum == None:
+        maximum = max(arr)
+    # create count list to store count value 
+    output = [0 for i in range(maximum)] 
+    # to store the result sorted array
+    result = [i for i in arr]  
+    count_idx = list(range(maximum))
+    count_lst = [0 for i in range(maximum)]
+    # count count_idx appearance in array and store the counts in corresponding count_lst
+    for i in count_idx:
+        count_lst[i] = arr.count(i)
+    # modify count by adding its previous count
+    for i in count_idx[:-1]:
+        count_lst[i+1] += count_lst[i]
+    
+    # Build the output count array
+    for i in range(len(arr)):
+        output[count_lst[(arr[i])]-1] = arr[i]
+        count_lst[arr[i]] -= 1
+    
+    # Copy the output array to arr, so that arr now
+    # contains sorted characters
+    for i in range(len(arr)):
+        result[i] = output[i]
+    return result
+    
 
-
-    return arr
+random.shuffle(array)
+print(array)
+print(counting_sort(array))
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,53 @@
+import random
+import time
+
 def linear_search(arr, target):
-    # Your code here
-
-
+    # given arr and target, return index of arr that arr[index]==target
+    idx = 0 
+    for item in arr:
+        if item == target:
+            return idx
+        idx +=1    
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    #Use binary search to return index of arr that arr[index]==target
+    # first sort the array 
+    lst_sorted = sorted(arr)
+    # initialize start, end and midpoint for search
+    start = 0
+    end = len(lst_sorted) - 1 
+    mid = (start+end)//2
+    found = 0
+    # while mid >1 and haven't found the target
+    while mid >= 1 and found == 0:        
+        # mid match target
+        if target == lst_sorted[mid]:
+            found = 1
+            return mid
+            
+        # target is smaller than mid, search the left part   
+        elif target < lst_sorted[mid]: 
+            end = mid         
+            
+        elif target > lst_sorted[mid]:
+            start = mid          
 
-    # Your code here
-
-
+        mid = (start+end)//2
     return -1  # not found
+
+
+lst = list(range(0, 100))
+random.shuffle(lst)
+target = 78
+print(lst)
+start_time = time.time()
+print(linear_search(lst, target))
+print(f'Runtime for linear search is {time.time()-start_time} seconds')
+
+
+start_time = time.time()
+print(binary_search(lst, target))
+print(f'Runtime for binary search is {time.time()-start_time} seconds')
